e40fac56e8df0a2fcddf1e1b730e800b12c5ff20
Renangr55 <yasmim.martins.profissional@gmail.com>
2025-12-17 16:40:33 -0300

    Já arrumo

---
+++ num10.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class num10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = new int[10];
+
+        // Ler os 10 números
+        System.out.println("Digite 10 números: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Número: " + (i) + ": ");
+            numeros[i] = sc.nextInt();
+
+        }
+
+        while (true) {
+            // Menu de opções
+            System.out.println("\nO que deseja saber?");
+            System.out.println("1. Quantos números pares");
+            System.out.println("2. Quantos números ímpares");
+            System.out.println("3. Quantos números positivos");
+            System.out.println("4. Quantos números negativos");
+            System.out.println("5. Encerrar");
+
+            System.out.println("\nEscolha uma das opções: ");
+            int opcao = sc.nextInt();
+
+            int contador = 0;
+
+            switch (opcao) {
+                case 1:
+                    for (int n : numeros) {
+                        if (n % 2 == 0) {
+                            contador++;
+                        }
+                    }
+                    System.out.println("Quantidade de números pares: " + contador);
+                    break;
+
+                case 2:
+                    for (int n : numeros) {
+                        if (n % 2 != 0) {
+                            contador++;
+                        }
+                    }
+                    System.out.println("Quantidade de números ímpares: " + contador);
+                    break;
+
+                case 3:
+                    for (int n : numeros) {
+                        if (n > 0) {
+                            contador++;
+                        }
+                    }
+                    System.out.println("Quantidade de números positivos: " + contador);
+                    break;
+
+                case 4:
+                    for (int n :numeros) {
+                        if (n < 0) {
+                            contador++;
+                        }
+                    }
+                    System.out.println("Quantidade de números negativos: " + contador);
+                    break;
+
+                case 5:
+                    System.out.println("Encerrando...");
+                    sc.close();
+                    break;
+
+                default:
+                    System.out.println("Opção não identificada");
+
+            }
+        }
+    }
+}
